2dba453359be46049586745bb8e069caceeb863c
Andrew <macculloch.andrew@gmail.com>
2020-11-01 12:10:49 -0400

    First draft of makefile

---
+++ cmake-build-debug-arm/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_9d39e.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_9d39e.dir/testCXXCompiler.cxx.obj"
+  "cmTC_9d39e.exe"
+  "cmTC_9d39e.exe.manifest"
+  "cmTC_9d39e.pdb"
+  "libcmTC_9d39e.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_9d39e.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
