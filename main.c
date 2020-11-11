19919e1ecd936c86d235e359939a67be38748360
Andrew <macculloch.andrew@gmail.com>
2020-11-11 14:31:18 -0400

    Working on Lab 6 - still working on pointers

--- main.c
+++ main.c
@@ -3,18 +3,24 @@
 #define RCC_IOPENR (unsigned long *)0x40021034
 
 #define GPIOA_MODER (unsigned long *)0x50000000
-#define GPIOA_ODR (unsigned long *)0x500000014
+#define GPIOA_ODR (unsigned long *)0x50000014
+#define GPIOC_MODER (unsigned long *)
+#define GPIOC_ODR (unsigned long *)
 uint8_t num = 0x55;
 uint8_t num2;
 
 void main()
 {
-/* enable register clock */
-    *RCC_IOPENR |= 0x00000001UL;
+/* enable register clocks for GPIOA and GPIOA */
+    *RCC_IOPENR |= 0x00000005UL;
 
     /* configure PA5 as output pin */
     *GPIOA_MODER &= ~0x00000C00UL;
     *GPIOA_MODER |= 0x00000400UL;
+
+    /* configure PC5 as output pin */
+    *GPIOC_MODER &= ~0x00000C00UL;
+    *GPIOC_MODER |= 0x00000400UL;
 
 	while(1)
 	{
