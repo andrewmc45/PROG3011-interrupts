46f6573110c6504ad773d0ac4583939296ace758
Andrew <macculloch.andrew@gmail.com>
2020-11-27 11:37:51 -0400

    Working on Lab 8 - clocks and timers blinking slowly

---
+++ cmake-build-debug-arm/CMakeFiles/sem.elf.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sem.elf.dir/main.c.obj"
+  "CMakeFiles/sem.elf.dir/startup.s.obj"
+  "sem.elf"
+  "sem.elf.manifest"
+  "sem.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/sem.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug-arm/CMakeFiles/sem.elf.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for sem.elf.
+# This may be replaced when dependencies are built.
